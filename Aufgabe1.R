db3b5d6b5dfa555c6296f5206828fa9fc76aa3d4
Theo Klautke <theoklautkemail@gmail.com>
2022-05-19 09:27:25 +0200

    Done :)

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -55,6 +55,25 @@ dat <- na.omit(dat)
 # Wie viele Teilnehmer/innen waren männlich (m), wie viele waren weiblich (w)?
 sexTable <- table(dat$Geschlecht)
 
-maleSum <- H[names(H) == 1]
-femaleSum <- H[names(H) == 2]
-variousSum <- H[names(H) == 3]
+maleSum <- sexTable[names(sexTable) == 1]
+femaleSum <- sexTable[names(sexTable) == 2]
+variousSum <- sexTable[names(sexTable) == 3]
+
+# Lassen Sie sich die Nutzungshäufigkeiten für Smartphones und Tablet PCs anzeigen. Was waren die
+# jeweils am häufigsten angegebenen Werte für die Nutzungshäufigkeiten dieser Geräte für das Studium?
+find_mode <- function(x) {
+    u <- unique(x)
+    tab <- tabulate(match(x, u))
+    u[tab == max(tab)]
+}
+
+modeSmartphone <- find_mode(dat$HaeufSmartphone) # Modus Rating = 3
+modeTablet <- find_mode(dat$HaeufTabletPC) # Modus Rating = 1
+
+# Erstellen Sie im Data Frame dat eine neue Spalte namens Geschwindigkeit, in der Sie für jede/n
+# Teilnehmer/in das Verhältnis von Entfernung und Fahrtdauer einspeichern.
+dat$Geschwindigkeit <- c(dat$Entfernung / dat$Fahrtdauer)
+
+# Berechnen Sie den Mittelwert der Geschwindigkeit Spalte getrennt für Frauen und Männer.
+speedMan <- mean(dat[dat$Geschlecht == 1, "Geschwindigkeit"])
+speedWoman <- mean(dat[dat$Geschlecht == 2, "Geschwindigkeit"])
